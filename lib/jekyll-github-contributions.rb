77b758931f635ec7abffcc4490835d674dbd0414
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-02-19 14:06:22 +0100

    Update jekyll-github-contributions.rb

--- lib/jekyll-github-contributions.rb
+++ lib/jekyll-github-contributions.rb
@@ -3,29 +3,29 @@ require 'net/http'
 require 'json'
 
 module Jekyll
-  # Generates a github contributions data file
-  class GithubContributionsGenerator < Jekyll::Generator
-    DATA_FILE = '_data/github-contributions.json'.freeze
+  # Generates a github issues data file
+  class GithubIssuesGenerator < Jekyll::Generator
+    DATA_FILE = '_data/github-issues.json'.freeze
     GITHUB_API_HOST = 'api.github.com'.freeze
-    CONTRIBUTIONS_URL = '/search/issues?q=type:pr+is:merged+author:%s&per_page=100&page=%i'.freeze
+    ISSUES_URL = '/search/issues?q=author:%s&per_page=100&page=%i'.freeze
 
     def generate(site)
       settings = {
         'cache' => 300,
         'page_limit' => 10
-      }.merge(site.config['githubcontributions'])
+      }.merge(site.config['githubissues'])
 
       return if File.exist?(DATA_FILE) && (File.mtime(DATA_FILE) + settings['cache']) > Time.now
-      Jekyll.logger.info 'Generating Github contributions data file'
+      Jekyll.logger.info 'Generating Github issues data file'
 
-      contributions = []
+      issues = []
 
       client = Net::HTTP.new(GITHUB_API_HOST, 443)
       client.use_ssl = true
 
       page = 1
       loop do
-        url = format(CONTRIBUTIONS_URL, settings['username'], page)
+        url = format(ISSUES_URL, settings['username'], page)
         response = client.get(url, 'Accept' => 'application/json')
         if response.code != '200'
           Jekyll.logger.warn "Cound not retrieve Github data: #{response.body}"
@@ -33,15 +33,15 @@ module Jekyll
         end
 
         results = JSON.parse(response.body)
-        contributions.concat(results['items'])
+        issues.concat(results['items'])
 
         break if page >= settings['page_limit'].to_i
-        break if contributions.length >= results['total_count']
+        break if issues.length >= results['total_count']
         page += 1
       end
 
       Dir.mkdir('_data') unless Dir.exist?('_data')
-      File.write(DATA_FILE, contributions.to_json)
+      File.write(DATA_FILE, issues.to_json)
     end
   end
 end
